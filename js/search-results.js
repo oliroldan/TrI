e91c47d0ec23b03af96fa32ebebcd95ece91309d
oliroldan <oroldan@udesa.edu.ar>
2023-11-14 10:18:04 -0300

    arreglando lo de la api

--- js/search-results.js
+++ js/search-results.js
@@ -1,7 +1,7 @@
 let qs =  location.search;
 let qsObj = new URLSearchParams(qs);
 let buscar = qsObj.get("buscar");
-let apiKey = "3df94698eaca4ce878e2c557de004fb2";
+let apiKey = "1173214cf5e2ac8f2c0ac1c242d0ec8a"
 let urlSearch = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${buscar}`;
 
 fetch(urlSearch)
@@ -18,20 +18,43 @@ fetch(urlSearch)
 
      for (let i = 0; i < peliculas.length; i++) {
         todasPelis += `<article class="portada">
-                            <a href="./detail-movie.html?idPelicula=${peliculas[i].id}">
+                            <a href="detail-movie.html?idPelicula=${peliculas[i].id}">
                             <img class="foto" src="https://image.tmdb.org/t/p/original${peliculas[i].poster_path}"></a>
-                            <h2>Película: <a href="./detail-movie.html?idPelicula=${peliculas[i].id}">${peliculas[i].title}</a></h2>
+                            <h2>Película: <a href="detail-movie.html?idPelicula=${peliculas[i].id}">${peliculas[i].title}</a></h2>
                         </article>`;
     }
     seccion.innerHTML = todasPelis;
     peliAgregada.innerText = buscar;
 
 })
-        
-      
 .catch(function(error){
     console.log(`El error es: ${error}`)
 })
 
+fetch(urlSearch)
+.then(function(response){
+    return response.json();
+   })
 
+.then(function(data){
+    console.log(data.results)
+     let series = data.results;
+     let seccion = document.querySelector(".cuadros-grupos");
+     let SerieAgregada = document.querySelector(".pelisearch"); 
+     let todasSeries = "";
+
+     for (let i = 0; i < series.length; i++) {
+        todasSeries += `<article class="portada">
+                            <a href="detail-serie.html?idPelicula=${series[i].id}">
+                            <img class="foto" src="https://image.tmdb.org/t/p/original${series[i].poster_path}"></a>
+                            <h2>Serie: <a href="detail-serie.html?idPelicula=${series[i].id}">${series[i].title}</a></h2>
+                        </article>`;
+    }
+    seccion.innerHTML = todasSeries;
+    SerieAgregada.innerText = buscar;
+
+})
+.catch(function(error){
+    console.log(`El error es: ${error}`)
+})
 
