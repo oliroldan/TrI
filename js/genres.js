c1c9efcba5a4d7a80710c0a978ffa8e5f1a9ebfe
sraimondo <sraimondo@udesa.edu.ar>
2023-11-16 15:50:41 -0300

    agregue la lista de generos de pelicualas y series

--- js/genres.js
+++ js/genres.js
@@ -1,49 +1,51 @@
 let ApiKey = "3df94698eaca4ce878e2c557de004fb2";
-let generosPeli = `https://api.themoviedb.org/3/genre/movie/list?api_key=${ApiKey}`;
-let generosSerie = `https://api.themoviedb.org/3/genre/tv/list?api_key=${ApiKey}`;
+let generosPeliURL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${ApiKey}`;
+let generosSerieURL = `https://api.themoviedb.org/3/genre/tv/list?api_key=${ApiKey}`;
 
-fetch(generosPeli)
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data.results)
-    let generos = data.results
-    let seccion = document.querySelector(".generosApiPeli")
-    let contenido = " "
-    for (let i = 0; i < generos.length ; i++){
-        contenido += `<article>
-                            <a href="./detail-genre.html?idPelicula=${generos[i].id}"></a>
-                            <h2><a href="./detail-genre.html?idPelicula=${generos[i].id}">${generos[i].title}</a></h2>
-                      </article>`;
-    }
-    
-    seccion.innerHTML = contenido;
-})
-.catch(function(error){
-    console.log(`El error es: ${error}`)
-});
+fetch(generosPeliURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.genres);
+        let generos = data.genres;
+        let seccion = document.querySelector(".generosApiPeli");
+        let contenido = " ";
+        for (let i = 0; i < generos.length; i++) {
+            contenido += `
+        <article>
+          <a href="./detail-genre.html?idGenero=${generos[i].id}&tipo=pelicula">
+            <h2>${generos[i].name}</h2>
+          </a>
+        </article>`;
+        }
 
+        seccion.innerHTML = contenido;
+    })
+    .catch(function (error) {
+        console.log(`El error es: ${error}`);
+    });
 
-fetch(generosSerie)
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data.results)
-    let generos2 = data.results
-    let seccion = document.querySelector(".generosApiSeries")
-    let contenido = " "
-    for (let i = 0; i < generos2.length; i++){
-        contenido += `<article>
-                            <a href="./detail-genre.html?idPelicula=${generos2[i].id}"></a>
-                            <h2><a href="./detail-genre.html?idPelicula=${generos2[i].id}">${generos2[i].title}</a></h2>
-                      </article>`;
-    }
-    
-    seccion.innerHTML = contenido;
-    
-})
-.catch(function(error){
-    console.log(`El error es: ${error}`)
-});
+fetch(generosSerieURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.genres);
+        let generos2 = data.genres;
+        let seccion = document.querySelector(".generosApiSeries");
+        let contenido = " ";
+        for (let i = 0; i < generos2.length; i++) {
+            contenido += `
+        <article>
+          <a href="./detail-genre.html?idGenero=${generos2[i].id}&tipo=serie">
+            <h2>${generos2[i].name}</h2>
+          </a>
+        </article>`;
+        }
+
+        seccion.innerHTML = contenido;
+    })
+    .catch(function (error) {
+        console.log(`El error es: ${error}`);
+    });
