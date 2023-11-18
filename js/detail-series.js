8ceb7f47d1b140933c69035f8401b5495f619483
joaquinaedo <jedo@udesa.edu.ar>
2023-11-17 22:25:34 -0300

    Modificaciones detalle sserie y extras

--- js/detail-series.js
+++ js/detail-series.js
@@ -4,7 +4,6 @@ let idSerie = urlParams.get('idSerie');
 let detalleSerie = `https://api.themoviedb.org/3/tv/${idSerie}?api_key=${ApiKey}&language=es`
 let recomendaciones = `https://api.themoviedb.org/3/tv/${idSerie}/recommendations?api_key=${ApiKey}&language=es`;
 
-
 fetch(detalleSerie)
   .then(function(response){
     return response.json();
@@ -21,7 +20,7 @@ fetch(detalleSerie)
           <li>Calificación: ${serie.vote_average}</li>
           <li>Fecha de estreno: ${serie.first_air_date}</li>
           <li>Sinopsis: ${serie.overview}</li>
-          <li>Géneros: ${obtenerGeneros(serie.genre_ids)}</li>
+          <li>Géneros: ${obtenerGeneros(serie.genres)}</li>
           <li class="boton"> <button class="verRecomendaciones">Ver Recomendaciones</button></li>
         </ul>
       </article>`;
@@ -30,47 +29,44 @@ fetch(detalleSerie)
 
     function obtenerGeneros(generos) {
       let enlacesGeneros = '';
-    
-      for (let i = 0; i < 6; i++) {
+
+      for (let i = 0; i < generos.length; i++) {
         let genero = generos[i];
         let enlace = `<a href="./detail-genres.html?idGenero=${genero.id}">${genero.name}</a>`;
-      
+
         enlacesGeneros += enlace;
-    
+
         if (i < generos.length - 1) {
           enlacesGeneros += ', ';
         }
       }
-    
+
       return enlacesGeneros;
     };
 
-    
-    let boton = document.querySelector('.verRecomendaciones');				
+    let boton = document.querySelector('.verRecomendaciones');
     boton.addEventListener('click', function() {
-      
+
       fetch(recomendaciones)
         .then(function(response) {
           return response.json();
         })
         .then(function(data) {
           console.log(data.results);
-          let recomendacion = data;
+          let recomendacion = data.results;
           let seccion1 = document.querySelector(".oculta");
           let recomendacionesHTML = "";
 
-          
           for (let i = 0; i < 5; i++) {
             recomendacionesHTML +=
               `<article>
-                <h3><a href="./detail-movie.html?idPelicula=${recomendacion[i].id}">${recomendacion[i].title}</a></h3>
-                <a href="./detail-movie.html?idPelicula=${recomendacion[i].id}">
+                <h3><a href="./detail-series.html?idSerie=${recomendacion[i].id}">${recomendacion[i].name}</a></h3>
+                <a href="./detail-series.html?idSerie=${recomendacion[i].id}">
                 <img class="foto" src="https://image.tmdb.org/t/p/original${recomendacion[i].poster_path}"></a>
               </article>`;
           }
-          
-          seccion1.innerHTML = recomendacionesHTML;
 
+          seccion1.innerHTML = recomendacionesHTML;
         })
         .catch(function(error) {
           console.log(`El error es: ${error}`);
